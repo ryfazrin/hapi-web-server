c957397b3430537266f4427cc3f91e4f96da8dd2
ryfazrin <riansera515@gmail.com>
2022-12-10 17:24:28 +0700

    created: file routes

---
+++ routes.js
@@ -0,0 +1,18 @@
+const rountes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Homepage!';
+    }
+  },
+  {
+    method: 'GET',
+    path: '/about',
+    handler: (request, h) => {
+      return 'About Page!';
+    }
+  }
+];
+
+module.exports = rountes;
