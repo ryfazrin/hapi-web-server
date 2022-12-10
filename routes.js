360f54eb77e7707902ce196c25ca90a09edc3928
ryfazrin <riansera515@gmail.com>
2022-12-10 17:28:39 +0700

    added: any routes

--- routes.js
+++ routes.js
@@ -7,12 +7,33 @@ const rountes = [
     }
   },
   {
+    method: '*',
+    path: '/',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat diakses dengan method tersebut';
+    },
+  },
+  {
     method: 'GET',
     path: '/about',
     handler: (request, h) => {
       return 'About Page!';
     }
-  }
+  },
+  {
+    method: '*',
+    path: '/about',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat diakses dengan method';
+    },
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: (request, h) => {
+      return 'Halaman tidak ditemukan';
+    },
+  },
 ];
 
 module.exports = rountes;
